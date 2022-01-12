cb6bcd6d4a4ef6d5f990e1a12e2db7d3d4167af7
leandro <leandrochagask@gmail.com>
2022-01-12 13:54:06 -0300

    feat(Table): logica para exibir retorno da api na tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <table border="1">
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-        <tr>
-          <td>linha</td>
-
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.map(({
+              id,
+              description,
+              tag,
+              method,
+              value,
+              currency,
+              exchangeRates,
+            }) => {
+              const ask = parseFloat(exchangeRates[currency].ask);
+              const curr = exchangeRates[currency].name.split('/')[0];
+              return (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ value }</td>
+                  <td>{ curr }</td>
+                  <td>{ ask.toFixed(2) }</td>
+                  <td>{ (value * ask).toFixed(2) }</td>
+                  <td>Real</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
